c80735bf3594e12f85a32b3496e8f7f722527403
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-07-17 23:12:33 +0859

    chore: add README and LICENSE

---
+++ README.md
@@ -0,0 +1,36 @@
+# Cached_property
+
+This lib made a Decorator like what in python.
+
+`cached_property` will only run once in the first time it is used.
+
+I do not know if it is useful. it is my first typescript Decorator.
+
+## Example
+
+```ts
+import cached_property from "@nobody/cached_property";
+import { assertEquals } from "jsr:@std/assert";
+
+class CacheTestClass {
+ constructor(public a: number) {
+ }
+
+ @cached_property
+ tag() {
+   return this.a + 10;
+ }
+}
+
+Deno.test(function testCache() {
+ const cached_test = new CacheTestClass(10);
+ assertEquals(cached_test.tag(), 20);
+ cached_test.a += 100;
+ assertEquals(cached_test.tag(), 20);
+
+ const cached_test_2 = new CacheTestClass(100);
+ assertEquals(cached_test_2.tag(), 110);
+ cached_test_2.a += 100;
+ assertEquals(cached_test_2.tag(), 110);
+});
+```
--- mod.ts
+++ mod.ts
@@ -1,10 +1,49 @@
-function cached_property(origin_Method: any, _context: DecoratorContext) {
-  let data: any = undefined;
+/**
+ * @module
+ * This module define a Decorator about cached_property property
+ * @example
+ *
+ * ```typescript
+ * import cached_property from "@nobody/cached_property";
+ * import { assertEquals } from "jsr:@std/assert";
+ *
+ * class CacheTestClass {
+ *  constructor(public a: number) {
+ *  }
+ *
+ *  @cached_property
+ *  tag() {
+ *    return this.a + 10;
+ *  }
+ * }
+ *
+ * Deno.test(function testCache() {
+ *  const cached_test = new CacheTestClass(10);
+ *  assertEquals(cached_test.tag(), 20);
+ *  cached_test.a += 100;
+ *  assertEquals(cached_test.tag(), 20);
+ *
+ *  const cached_test_2 = new CacheTestClass(100);
+ *  assertEquals(cached_test_2.tag(), 110);
+ *  cached_test_2.a += 100;
+ *  assertEquals(cached_test_2.tag(), 110);
+ * });
+ * ```
+ */
+
+export default function cached_property<T>(
+  origin_Method: T,
+  context: DecoratorContext,
+): T {
+  const name = context.name as string;
+  const cached_data_name: string = "_cached_" + name;
+  // deno-lint-ignore no-explicit-any
   function replacementMethod(this: any, ...args: any[]) {
-    if (data === undefined) {
-      data = origin_Method.call(this, ...args);
+    if (this[cached_data_name] === undefined) {
+      // deno-lint-ignore no-explicit-any
+      this[cached_data_name] = (origin_Method as any).call(this, ...args);
     }
-    return data;
+    return this[cached_data_name];
   }
-  return replacementMethod;
+  return replacementMethod as T;
 }
